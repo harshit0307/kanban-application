829c451de8a85ed5028038135fe2701a03212beb
harshit0307 <63691792+harshit0307@users.noreply.github.com>
2024-01-28 20:46:54 +0530

    Delete src directory

--- src/api/sectionApi.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient";
-
-const sectionApi = {
-    create: (boardID) => axiosClient.post(`boards/${boardID}/sections`),
-    delete: (boardID, sectionId) => axiosClient.delete(`boards/${boardID}/sections/${sectionId}`),
-    update: (boardID, sectionId, params) => axiosClient.put(`boards/${boardID}/sections/${sectionId}`, params)
-}
-
-export default sectionApi
